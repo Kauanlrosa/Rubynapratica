e8f7adebb556c4f91cbe720eb6101fc64265a4a7
Kauan Rosa <kauanonlainebr@gmail.com>
2025-11-22 11:15:16 -0300

    Missão 1 realizada com sucesso !!
    utilizei os comandos print para realizar a saída de dados e os comandos gets.to_i e gets.chomp para a entrada de dados.
    
    Realizei um programa onde o computador pede para o usuário digitar seu nome  e logo em seguida o seu sobrenome, por fim a idade, fazendo com que o resultado esperado seja a resposta desses comandos realizadas pelo usuário.

---
+++ missao1.rb
@@ -0,0 +1,20 @@
+# comando de saída de dados.
+print 'Digite o seu nome: ' 
+
+# comando de entrada de dados.
+nome = gets.chomp
+
+# comando de saída de dados.
+print 'Digite o seu sobrenome: '
+
+# comando de entrada de dados.
+sobrenome = gets.chomp
+
+# comando de saída de dados.
+print 'Digite a sua idade: '
+
+# comando de entrada de dados.
+idade = gets.to_i
+
+# comando de saída de dados utilizando o puts.
+puts "Você tem #{idade} anos de idade e se chama #{nome} #{sobrenome} "
